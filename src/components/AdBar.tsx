394320aa0c8d7e094fd8a713c3549dd951d5a9c8
dongyeon.park <yeon0729@sookmyung.ac.kr>
2025-12-25 20:17:55 +0900

    chore: comment out ad placeholder text in AdBar component.

--- src/components/AdBar.tsx
+++ src/components/AdBar.tsx
@@ -4,7 +4,7 @@ const AdBar = () => {
     return (
         <div className="ad-bar-container">
             <div className="ad-placeholder">
-                ADVERTISEMENT AREA (Google AdSense / Kakao AdFit)
+                {/* ADVERTISEMENT AREA (Google AdSense / Kakao AdFit) */}
             </div>
         </div>
     );
